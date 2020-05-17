32b6e48d2c3f17837a80f865ab3a6a7a424e938f
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-16 17:22:37 -0700

    iOS

--- AnTubeApp/HomeScreenViewController.swift
+++ AnTubeApp/HomeScreenViewController.swift
@@ -10,28 +10,30 @@ import UIKit
 
 
 class HomeScreenViewController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-
-      collectionView.backgroundColor = .orange
-
-      navigationItem.title = "Home"
-      collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
-    }
-
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+    collectionView.backgroundColor = .orange
+    
+    navigationItem.title = "Home"
+    collectionView.register(VideoCell.self, forCellWithReuseIdentifier: "Cell")
+  }
+  
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return 5
   }
-   
+  
   
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//    let cell = UICollectionViewCell()
+    //    let cell = UICollectionViewCell()
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
     
     
-    cell.backgroundColor = .brown
+//        cell.backgroundColor = .brown
+    
+    
     return cell
   }
   
@@ -39,5 +41,43 @@ class HomeScreenViewController: UICollectionViewController,UICollectionViewDeleg
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     return CGSize(width: view.frame.width, height: 200)
   }
+  
+  
+  
+  
+  
+  
+  
+  
+}
 
+//MARK: - Custom Video Cell
+class VideoCell: UICollectionViewCell {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupViews()
+  }
+  
+  
+  var thumbnailImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.backgroundColor = .cyan
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+  
+  
+  func setupViews(){
+        addSubview(thumbnailImageView)
+    
+    addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: [], metrics: nil, views: ["v0" : thumbnailImageView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-16-[v0]-16-|", options: [], metrics: nil, views: ["v0" : thumbnailImageView]))
+    
+//    thumbnailImageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+    
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 }
